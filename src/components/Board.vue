55adaa32af2798e661fa9bcdd2a92d70e601ec27
Jun Yu <shockz@me.com>
2020-09-30 13:03:06 +0900

    card 리스팅 및 라우팅 수정중

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,6 +2,14 @@
 div
   div Board
   span bid: {{ bid }}
+  div
+    router-link(:to="`/b/${bid}/c/1`") Card 1
+    span
+    = ' '
+    router-link(:to="`/b/${bid}/c/2`") Card 2
+    span
+    = ' '
+    router-link(:to="`/b/${bid}/c/3`") Card 3
   hr
   router-view
 </template>
