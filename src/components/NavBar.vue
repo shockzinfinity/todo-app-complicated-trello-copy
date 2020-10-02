564b5f6d93cfac12d126a5836d8c62d0f3211393
Jun Yu <shockz@me.com>
2020-10-02 22:29:16 +0900

    헤더 설정

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
-div
-  router-link(to="/") Home
-  span
-  = ' '
-  router-link(to="/login") Login
+nav.header
+  .header-logo
+    router-link(to="/") Home
+  .header-auth
+    router-link(to="/login") Login
+  //- span
+  //- = ' '
 </template>
 
 <script>
@@ -11,4 +13,52 @@ export default {};
 </script>
 
 <style>
+.header {
+  flex: none;
+  background-color: rgba(0, 0, 0, 0.15);
+  height: 32px;
+  padding: 4px;
+}
+
+.header a {
+  display: block;
+  height: 30px;
+  line-height: 30px;
+  text-decoration: none;
+  color: rgba(255, 255, 255, 0.5);
+}
+
+.header-logo {
+  position: absolute;
+  left: 50%;
+  top: 7px;
+  margin-left: -30px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 24px;
+}
+
+.header-logo a:hover,
+.header-logo a:focus {
+  color: rgba(255, 255, 255, 0.9);
+}
+
+.header-auth {
+  position: absolute;
+  right: 15px;
+  top: 5px;
+}
+
+.header-auth a {
+  border-radius: 2px;
+  padding: 0 10px;
+  background-color: rgba(255, 255, 255, 0.5);
+  color: white;
+  transition: all 0.3s;
+}
+
+.header-auth a:hover,
+.header-auth a:focus {
+  background-color: rgba(255, 255, 255, 0.3);
+}
 </style>
