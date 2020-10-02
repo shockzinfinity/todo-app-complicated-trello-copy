bf957f03077381b5f73d7371e4c76cb4088622a3
Jun Yu <shockz@me.com>
2020-10-02 19:56:13 +0900

    browser api 를 이용한 api call

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,8 @@
 <template lang="pug">
 div Home
   div Board List:
+    div(v-if="loading") Loading...
+    div(v-else) API response: {{ apiRes }}
     ul
       li
         router-link(to="/b/1") Board 1
@@ -11,7 +13,35 @@ div Home
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      apiRes: "",
+      loading: false
+    };
+  },
+  methods: {
+    fetchData() {
+      this.loading = true;
+
+      const req = new XMLHttpRequest();
+
+      req.open("GET", "https://localhost:4001/weatherforecast");
+      req.send();
+      req.addEventListener("load", () => {
+        this.loading = false;
+        this.apiRes = {
+          status: req.status,
+          statusText: req.statusText,
+          response: JSON.parse(req.response)
+        };
+      });
+    }
+  },
+  created() {
+    this.fetchData();
+  }
+};
 </script>
 
 <style>
